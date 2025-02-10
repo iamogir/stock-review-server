43404057e1d6ca400bc3c4ae6862e5c851f7a664
iamogir <ia.mogirevsky@gmail.com>
2025-02-10 17:34:09 +0200

    made connection from .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,24 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
-import { ProductModule } from './product/product.module';
+// import { MongooseModule } from '@nestjs/mongoose';
+// import { ProductModule } from './product/product.module';
 import { APP_FILTER, BaseExceptionFilter } from '@nestjs/core';
 import { TestModule } from './test/test.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 // import mongoose from 'mongoose';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/testbd'),
-    ProductModule,
+    ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env' }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_URI'),
+      }),
+      inject: [ConfigService],
+    }),
+    // MongooseModule.forRoot('mongodb://localhost:27017/testbd'),
+    // ProductModule,
     TestModule,
   ],
   providers: [{ provide: APP_FILTER, useClass: BaseExceptionFilter }],
