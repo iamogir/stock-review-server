19b288eb0cc3326e4471b8e7e745ebfe910c2cd9
iamogir <ia.mogirevsky@gmail.com>
2025-03-04 16:22:31 +0200

    changed product schema

--- src/product/schema/product.schema.ts
+++ src/product/schema/product.schema.ts
@@ -1,19 +1,10 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { Document } from 'mongoose';
 
 @Schema({ collection: 'products' })
 export class Product extends Document {
   @Prop({ required: true }) name: string;
   @Prop({ required: true }) category: string;
-  @Prop({ required: true }) weight: number;
-  @Prop({ required: true }) unitWeight: string;
-  @Prop({ required: true }) quantityUnits: number;
-  @Prop({ required: true }) expirationDate: Date;
-  @Prop({ required: true }) createdAt: Date;
-  @Prop({ required: true }) updatedAt: Date;
-  @Prop({ required: false }) barcode: string;
-  @Prop({ required: true }) supplier: string;
-  @Prop({ required: true }) storageLocation: string;
+  @Prop({ required: false }) brand: string;
   @Prop({ required: true }) status: boolean;
 }
 
