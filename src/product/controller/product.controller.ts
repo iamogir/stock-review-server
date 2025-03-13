8d8359a058b086e81b6210a17d44411364df4e7d
iamogir <ia.mogirevsky@gmail.com>
2025-03-13 15:59:59 +0200

    add new product controller

--- src/product/controller/product.controller.ts
+++ src/product/controller/product.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { ProductService } from '../service/product.service';
 import { ProductDto } from '../dto/product.dto';
 
@@ -9,5 +9,9 @@ export class ProductController {
   @Get('/get_all_products')
   async getAllProducts(): Promise<ProductDto[]> {
     return this.productService.getAllProducts();
+  }
+  @Post('/add_new_product')
+  async addNewProduct(@Body() product: ProductDto): Promise<ProductDto> {
+    return this.productService.addNewProduct(product);
   }
 }
