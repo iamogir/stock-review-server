10045bcd72d3977d80b5aa87f16a50470160f5d6
iamogir <ia.mogirevsky@gmail.com>
2025-04-03 21:18:32 +0300

    productHelper in product controller

--- src/product/controller/product.controller.ts
+++ src/product/controller/product.controller.ts
@@ -1,10 +1,14 @@
 import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
 import { ProductService } from '../service/product.service';
 import { ProductDto } from '../dto/product.dto';
+import { ProductHelperService } from '../../productHelper/service/productHelper.service';
 
 @Controller('products')
 export class ProductController {
-  constructor(private readonly productService: ProductService) {}
+  constructor(
+    private readonly productService: ProductService,
+    private readonly productHelperService: ProductHelperService,
+  ) {}
 
   @Get('/get_all_products')
   async getAllProducts(): Promise<ProductDto[]> {
@@ -19,7 +23,9 @@ export class ProductController {
     return this.productService.addNewProductsStack(products);
   }
   @Delete('/delete_product_by_id/:id')
-  async deleteProductById(@Param('id') id: string): Promise<{ id: string; count: number }> {
-    return this.productService.deleteProductById(id);
+  async deleteProductById(
+    @Param('id') id: string,
+  ): Promise<{ id: string; count: number }> {
+    return this.productHelperService.deleteProductById(id);
   }
 }
